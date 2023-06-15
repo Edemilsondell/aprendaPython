a7ffc71a99a10cb158e02a9aed7cc0ac7dc1d75b
Edemilson da Mata <edemilsondell@hotmail.com>
2023-06-14 23:33:19 -0400

    Aprendendo sobr listas

---
+++ pythonProject/listas.py
@@ -0,0 +1,40 @@
+print()
+
+"""
+Lista em python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+conhecimentos reutilizáveis - indices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+
+"""
+
+#..............+01234
+#..............-54321
+string = "ABCDE" # 5 caracteres
+lista1 = []
+lista1.append(string)
+print(lista1)
+print(lista1[0])
+# print(lista1[1])
+# print(lista1[2])
+
+
+
+#print(bool([])) ..#false
+# print(lista, type(lista)) #..[] <class 'list'>
+#.........0.....1.......2......3....4
+#.........-5...-4......-3.....-2....-1
+lista = [123, True, "Ednick", 1.2, []]
+
+print(lista[2])
+print(lista[-3])
+print(lista[0], type(lista[0]))
+print(lista[1], type(lista[1]))
+print(lista[2], type(lista[2]))
+print(lista[3], type(lista[3]))
+print(lista[4], type(lista[4]))
+
+
+
+
